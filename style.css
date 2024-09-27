fc1e67e14fbfd157556e23f004f43a6c28cd41f3
Rafael <117850423+RafaImbroxavel@users.noreply.github.com>
2024-09-27 16:57:33 -0300

    style.css

---
+++ style.css
@@ -0,0 +1,38 @@
+body {
+    background-color: black;
+    color: white;
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #003366; /* Azul escuro */
+    padding: 20px;
+    text-align: center;
+}
+
+main {
+    padding: 20px;
+}
+
+h1, h2 {
+    color: #00ccff; /* Azul claro */
+}
+
+canvas {
+    max-width: 1000px; /* Aumentando o tamanho do gráfico */
+    max-height: 400px;
+    margin: auto;
+    display: block;
+    border: 2px solid #00ccff; /* Borda do gráfico */
+    border-radius: 10px; /* Bordas arredondadas */
+    box-shadow: 0 4px 15px rgba(0, 204, 255, 0.5); /* Sombra */
+}
+
+section {
+    margin-top: 20px;
+    background-color: rgba(0, 51, 102, 0.8); /* Fundo semitransparente para a seção */
+    padding: 15px;
+    border-radius: 10px; /* Bordas arredondadas */
+}
